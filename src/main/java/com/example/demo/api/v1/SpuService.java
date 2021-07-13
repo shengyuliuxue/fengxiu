b3490018553ea997b767fcdbcc6ce524c910332f
shengyu <594549026@qq.com>
2021-07-13 14:39:35 +0800

    long

--- src/main/java/com/example/demo/api/v1/SpuService.java
+++ src/main/java/com/example/demo/api/v1/SpuService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Service
 public class SpuService {
     @Autowired SpuRepository spuRepository;
-   public Optional<Spu> getSpu(Integer id){
+   public Optional<Spu> getSpu(Long id){
        return spuRepository.getSpuById(id);
    }
 
