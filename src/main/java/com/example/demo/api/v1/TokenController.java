a3039ef2ab2ca193564e782e5694a16af0471db7
shengyu <594549026@qq.com>
2021-09-22 22:10:01 +0800

    error  fixed

--- src/main/java/com/example/demo/api/v1/TokenController.java
+++ src/main/java/com/example/demo/api/v1/TokenController.java
@@ -1,7 +1,10 @@
 package com.example.demo.api.v1;
 
+import com.example.demo.exception.http.ServerErrorException;
+import com.example.demo.util.JWTUtil;
 import com.example.demo.util.MapAndJson;
 import com.example.demo.vo.UserVO;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,15 +15,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class TokenController {
-
+    @Autowired ObjectMapper  mapper;
     @PostMapping(path="v1/token", consumes = "application/json", produces = "application/json")
-    public Map<String, String> getToken(@RequestBody UserVO account){
+    public Map<String,String> getToken(@RequestBody UserVO account){
         //appid:  wxe020efca01975f5b
         //Appsecret: 83eda50dea97ce05efcce515b29f989e
         //code: account
@@ -34,17 +38,30 @@ public class TokenController {
 
         Map<String, String> resultmap  = new HashMap<>();
         wexinLogResult result = new wexinLogResult();
-        MapAndJson<wexinLogResult> mapAndJson = new MapAndJson<>();
-        String resultStr = restTemplate.getForObject(url,String.class,map);
-        mapAndJson.convertToEntityAttribute(resultStr);
-        System.out.println();
 
-       // tokenMap.put("token", result);
-        return null;
+        String resultStr = restTemplate.getForObject(url,String.class,map);
+        //System.out.println(resultStr);
+        try {
+            wexinLogResult wr = mapper.readValue(resultStr, wexinLogResult.class);
+            String openid = wr.getOpenid();
+            String token = JWTUtil.CreateToken(openid);
+            //{openid=ojOM646Uf1MtC6GtrDPDCKb3CtsE}
+            //System.out.println(token);
+            resultmap.put("token", token);
+            return resultmap;
+        }catch(JsonProcessingException e) {
+            e.printStackTrace();
+            throw new ServerErrorException(9999);
+        }
+
     }
 
+    @PostMapping(path="v1/token/verify", consumes = "application/json", produces = "application/json")
+    public String verifyToken(@RequestBody String jsonStr){
 
-
+        System.out.println(jsonStr);
+        return jsonStr;
+    }
 
 }
 
